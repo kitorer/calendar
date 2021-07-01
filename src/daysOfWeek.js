7cb3aa60ae5a43a6a08e90f856342f3b332f3c0b
kim <vikkhuang1234@gmail.com>
2021-07-01 16:21:53 -0400

    weekname grid

--- src/daysOfWeek.js
+++ src/daysOfWeek.js
@@ -1,8 +1,17 @@
+import './daysOfWeek.css'
 
 function Day(){
 	return(
 		<header>
-			<h1>Sunday  Monday    Tuesday    Wednesday   Thursday  Friday  Saturday</h1>
+			<div class ="wrapper">
+				<h1 class = "weeknameClass1">Sunday </h1>
+				<h1 class = "weeknameClass2">Monday </h1>
+				<h1 class = "weeknameClass3">Tuesday </h1>
+				<h1 class = "weeknameClass4">Wednesday </h1>
+				<h1 class = "weeknameClass5">Thursday </h1>
+				<h1 class = "weeknameClass6">Friday </h1>
+				<h1 class = "weeknameClass7">Saturday </h1>
+			</div>
 		</header>
 	)
 }
